6b020cf872ba6bdc7c489c24eb33873a080cc126
Geletsky <nsurcov98@gmail.com>
2023-12-02 16:54:13 +0300

    JS tasks completed - HW #2

---
+++ JS/homework - #2/script.js
@@ -0,0 +1,219 @@
+// TASK 1
+
+// let a = 7;
+// let b = 9;
+// result = a * b;
+// console.log(result);
+
+// TASK 2
+
+// let c = 7;
+// let d = 9;
+// result = c / d;
+// console.log(result);
+
+// TASK 3
+
+// let e = 3;
+// let f = 5;
+// result = e + f;
+// console.log(result);
+
+// TASK 4
+
+// let e1 = '3';
+// let f1 = 5;
+// result = e1 + f1;
+// console.log(result);
+
+// TASK 5
+
+// let e2 = 3;
+// let f2 = 0;
+// result = e2 / f2;
+// console.log(result);
+
+// TASK 6
+
+// let e3 = 3;
+// let f3 = 'Hello';
+// result = e3 + f3;
+// console.log(result);
+
+// TASK 7
+
+// let e4 = 3;
+// let f4 = 'Hello';
+// result = e4 * f4;
+// console.log(result);
+
+// TASK 8
+
+// function t8() {
+//    const button = document.querySelector('.b-8');
+//    const block = document.querySelector('.out-8');
+
+//    button.addEventListener('click', () => block.innerText = 'Кликнули!');
+// }
+
+// t8();
+
+// TASK 9
+
+// function t9() {
+//    const input = document.querySelector('.i-9');
+//    const button = document.querySelector('.b-9');
+//    const block = document.querySelector('.out-9');
+
+//    button.addEventListener('click', () => block.innerText = input.value);
+// }
+
+// t9();
+
+// TASK 10
+
+// function t10() {
+//    const input = document.querySelector('.i-10');
+//    const button = document.querySelector('.b-10');
+//    const block = document.querySelector('.out-10');
+
+//    button.addEventListener('click', () => block.innerText = input.value * 20);
+// }
+
+// t10();
+
+// TASK 11
+
+// function t11() {
+//    const input = document.querySelector('.i-11');
+//    const button = document.querySelector('.b-11');
+//    const block = document.querySelector('.out-11');
+
+//    button.addEventListener('click', () => block.innerText = input.value + 55);
+// }
+
+// t11();
+
+// TASK 12
+
+// function t12() {
+//    const inputOne = document.querySelector('.i-12-1');
+//    const inputTwo = document.querySelector('.i-12-2');
+//    const button = document.querySelector('.b-12');
+//    const block = document.querySelector('.out-12');
+
+//    button.addEventListener('click', () => block.innerText = inputOne.value + ' ' + inputTwo.value);
+// }
+
+// t12();
+
+// TASK 13
+
+// function t13() {
+//    const inputOne = document.querySelector('.i-13-1');
+//    const inputTwo = document.querySelector('.i-13-2');
+//    const button = document.querySelector('.b-13');
+//    const block = document.querySelector('.out-13');
+
+//    button.addEventListener('click', () => {
+//       const a = parseFloat(inputOne.value);
+//       const b = parseFloat(inputTwo.value);
+
+//       block.innerText = a + b;
+//   });
+// }
+
+// t13();
+
+// TASK 14
+
+// function t14() {
+//    const input = document.querySelector('.i-14');
+//    const button = document.querySelector('.b-14');
+
+//    button.addEventListener('click', () => input.value = 'Go');
+// }
+
+// t14();
+
+// TASK 15
+
+// function t15() {
+//    const input = document.querySelector('.i-15');
+//    const button = document.querySelector('.b-15');
+
+//    button.addEventListener('click', () => input.style.border = "4px solid red");
+// }
+
+// t15();
+
+// TASK 16
+
+// function t16() {
+//    const inputOne = document.querySelector('.i-16-1');
+//    const inputTwo = document.querySelector('.i-16-2');
+//    const button = document.querySelector('.b-16');
+//    const block = document.querySelector('.out-16');
+
+//    button.addEventListener('click', () => block.innerText = inputOne.value + inputTwo.value);
+// }
+
+// t16();
+
+// TASK 17
+
+// function t17() {
+//    const input = document.querySelector('.i-17');
+//    const button = document.querySelector('.b-17');
+
+//    button.addEventListener('click', () => console.log(parseFloat(input.value)));
+// }
+
+// t17();
+
+// TASK 18
+
+// function t18() {
+//    const a = document.querySelector('.i-18');
+//    const button = document.querySelector('.b-18');
+//    const block = document.querySelector('.out-18');
+
+//    button.addEventListener('click', () => block.innerText = parseFloat(a.value))
+
+// }
+
+// t18();
+
+// TASK 19
+
+// function t19() {
+//    const button = document.querySelector('.b-19');
+//    const block = document.querySelector('.out-19');
+
+//    button.addEventListener('click', () => {
+//       const inputOne = document.querySelector('.i-19-1').value;
+//       const inputTwo = document.querySelector('.i-19-2').value;
+//       const parsedInputOne = parseFloat(inputOne);
+//       const parsedInputTwo = parseFloat(inputTwo);
+
+//       block.innerText = parsedInputOne + parsedInputTwo;
+
+//    });
+// }
+
+// t19();
+
+// TASK 20
+
+// function t20() {
+//    const button = document.querySelector('.b-20');
+//    const block = document.querySelector('.out-20');
+//    let clickCount = 0;
+
+//    button.addEventListener('click', () => {
+//       clickCount++;
+//       block.innerText = clickCount;
+//    })
+// }
+
+// t20();
