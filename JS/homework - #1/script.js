fe352e55848d350c2052dace8fa570230e5da6a2
Geletsky <nsurcov98@gmail.com>
2023-12-02 14:51:58 +0300

    JS tasks completed - HW #1 - string concatenation - using alert - the operation of subtracting numbers - changing the content of page elements - working with innerHTML and textContent

---
+++ JS/homework - #1/script.js
@@ -0,0 +1,104 @@
+// #1
+
+// console.log('Nikita');
+
+// #2
+
+// console.log(12);
+
+// #3
+
+// console.log('Добро ' + 'пожаловать' + ' на курс');
+
+// #4
+
+// alert(2019);
+
+// #5
+
+// console.log(2019 - 200);
+
+// #6
+
+// const text = document.getElementById('six');
+// text.innerText = 'Hello World';
+
+// #7
+
+// const text = document.getElementById('seven');
+// result = 12 * 12;
+// text.innerHTML = result;
+
+// #8
+
+// const text = document.querySelector('.out-8');
+// text.innerText = 'task-8';
+
+// #9
+
+// const text = document.querySelector('.out-10');
+// text.innerHTML = '<h2>Hi</h2>';
+
+// #10
+
+// const text = document.querySelector('.out-11');
+// text.innerHTML = '123';
+// text.innerHTML += '456';
+
+// #11
+
+// const a = document.querySelector('.out-12');
+// a.innerHTML = 3.1415;
+
+// #12
+
+// const out13 = document.querySelector('.out-13');
+// out13.innerHTML = '<img src="https://static.wikia.nocookie.net/rustarwars/images/d/d6/Yoda_SWSB.png/revision/latest?cb=20171222112613" alt="js">';
+
+// #13 
+
+// z1 = 6;
+// z2 = 3;
+// result = z1 * z2;
+
+// const text = document.querySelector('.out-14');
+// text.innerHTML = result;
+
+// #14
+
+// y1 = 6;
+// y2 = 3;
+// result = y1 / y2;
+
+// const text = document.querySelector('.out-15');
+// text.innerHTML = result;
+
+// #15
+
+// x1 = 'Hello';
+// x2 = 5;
+// result = x1 + x2;
+
+// const text = document.querySelector('.out-16');
+// text.innerHTML = result;
+
+// #16
+
+// const out17 = document.querySelector('.out-17');
+// console.log(out17);
+
+// #17 и #18
+
+// let out18 = document.querySelector('.out-17');
+// console.log(out18);
+
+// out18 = 5;
+// console.log(out18);
+
+// out18 = document.querySelector('.out-19-test');
+// console.log(out18);
+
+// #19
+
+// const out20 = document.querySelector('.out-15');
+// out20.textContent = '<h2>Hi</h2>';
